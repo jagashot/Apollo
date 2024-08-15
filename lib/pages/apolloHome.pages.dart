ec8f15d6496e1af1c08b25e30b49f0212571e705
YehudaDaniel <h10ytguf@gmail.com>
2024-08-15 22:15:46 +0300

    setting the home page up for refactorization

--- lib/pages/apolloHome.pages.dart
+++ lib/pages/apolloHome.pages.dart
@@ -1,3 +1,4 @@
+import 'package:apollo_poc/widgets/buildDot.widgets.dart';
 import 'package:apollo_poc/widgets/historyitem.widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
@@ -12,7 +13,7 @@ class ApolloHome extends StatefulWidget {
 
 class _ApolloHomeState extends State<ApolloHome> {
   int currentIndex = 1;
-  final PageController _controller = PageController(initialPage: 1);
+  final PageController _controller = PageController(initialPage: 1); //Sets the initial page
   Uint8List? _fileBytes; // Variable to hold the selected file bytes
   String? _fileName; // Variable to hold the selected file name
 
@@ -21,6 +22,175 @@ class _ApolloHomeState extends State<ApolloHome> {
     _controller.dispose();
     super.dispose();
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        // background Image for the whole application
+        decoration: const BoxDecoration(
+          image: DecorationImage(image: AssetImage('assets/photos/app_background.jpeg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        // ----------------------------------------------------------------
+        // 3 Dots indicator
+        child: Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).viewPadding.top, // height of the status bar for every phone separately
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: List.generate(3, (index) => buildDot(currentIndex, index, context)),
+            ),
+        // ----------------------------------------------------------------
+            Expanded(
+              child: PageView(
+                controller: _controller,
+                onPageChanged: (int index) {
+                  setState(() {
+                    currentIndex = index;
+                  });
+                },
+                children: [
+                  // History View for the first page
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 30,
+                      vertical: 15,
+                    ),
+                    child: Column(
+                      children: [
+                        Expanded(
+                          child: ListView(
+                            children: [
+                              Container(
+                                margin: const EdgeInsets.only(
+                                  top: 50,
+                                  bottom: 20,
+                                ),
+                                child: const Text(
+                                  'History',
+                                  style: TextStyle(
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                              HistoryItem(),
+                              HistoryItem(),
+                              HistoryItem(),
+                              HistoryItem(),
+                              HistoryItem(),
+                              HistoryItem(),
+                              HistoryItem(),
+                              HistoryItem(),
+                              HistoryItem(),
+                              HistoryItem(),
+                              HistoryItem(),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  // ----------------------------------------------------------------
+                  //Record View for the second page
+                  Container(
+                    height: MediaQuery.of(context).size.height,
+                    width: MediaQuery.of(context).size.width,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'Tap to Record',
+                          style: TextStyle(color: Colors.white, fontSize: 20, decoration: TextDecoration.none),
+                        ),
+                        const SizedBox(
+                          height: 40,
+                        ),
+                        GestureDetector(
+                          onTap: () => print("Yea!"),
+                          child: Material(
+                            shape: const CircleBorder(),
+                            elevation: 8,
+                            child: Container(
+                              padding: const EdgeInsets.all(40),
+                              height: 200,
+                              width: 200,
+                              decoration: const BoxDecoration(
+                                image: DecorationImage(image:AssetImage('assets/photos/main_btn.png'), 
+                                fit: BoxFit.fill
+                                ),
+                                shape: BoxShape.circle,
+                                color: Color.fromARGB(255, 140, 80, 182),
+                              ),
+                              child: const Icon(
+                                Icons.mic,
+                                color: Colors.white,
+                                size: 100,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  // ----------------------------------------------------------------
+                  Container(
+                    height: MediaQuery.of(context).size.height,
+                    width: MediaQuery.of(context).size.width,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'Upload File',
+                          style: TextStyle(color: Colors.white, fontSize: 20, decoration: TextDecoration.none),
+                        ),
+                        const SizedBox(
+                          height: 40,
+                        ),
+                        GestureDetector(
+                          onTap: _openFileExplorer,
+                          child: Material(
+                            shape: const CircleBorder(),
+                            elevation: 8,
+                            child: Container(
+                              padding: const EdgeInsets.all(40),
+                              height: 200,
+                              width: 200,
+                              decoration: const BoxDecoration(
+                                image: DecorationImage(image:AssetImage('assets/photos/main_btn.png'), 
+                                  fit: BoxFit.fill
+                                ),
+                                shape: BoxShape.circle,
+                                color: Color.fromARGB(255, 140, 80, 182),
+                              ),
+                              child: const Icon(
+                                Icons.add,
+                                color: Colors.white,
+                                size: 100,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Text('safasfasfasfasfsafs'),
+          ],
+        ),
+      ),
+    );
+  }
+
 
   void _openFileExplorer() async {
     try {
@@ -50,167 +220,5 @@ class _ApolloHomeState extends State<ApolloHome> {
     // You can use this variable to pass the file to another program
     final mp3File = fileBytes;
     print('File saved as $fileName with ${mp3File.lengthInBytes} bytes.');
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 89, 0, 89),
-      body: Column(
-        children: [
-          SizedBox(
-            height: MediaQuery.of(context).viewPadding.top, // height of the status bar for every phone separately
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: List.generate(3, (index) => buildDot(index, context)),
-          ),
-          Expanded(
-            child: PageView(
-              controller: _controller,
-              onPageChanged: (int index) {
-                setState(() {
-                  currentIndex = index;
-                });
-              },
-              children: [
-                Container(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 30,
-                    vertical: 15,
-                  ),
-                  child: Column(
-                    children: [
-                      Expanded(
-                        child: ListView(
-                          children: [
-                            Container(
-                              margin: const EdgeInsets.only(
-                                top: 50,
-                                bottom: 20,
-                              ),
-                              child: const Text(
-                                'History',
-                                style: TextStyle(
-                                  fontSize: 30,
-                                  fontWeight: FontWeight.w500,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ),
-                            HistoryItem(),
-                            HistoryItem(),
-                            HistoryItem(),
-                            HistoryItem(),
-                            HistoryItem(),
-                            HistoryItem(),
-                            HistoryItem(),
-                            HistoryItem(),
-                            HistoryItem(),
-                            HistoryItem(),
-                            HistoryItem(),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  height: MediaQuery.of(context).size.height,
-                  width: MediaQuery.of(context).size.width,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const Text(
-                        'Tap to Record',
-                        style: TextStyle(color: Colors.white, fontSize: 20, decoration: TextDecoration.none),
-                      ),
-                      const SizedBox(
-                        height: 40,
-                      ),
-                      GestureDetector(
-                        onTap: () => print("Yea1"),
-                        child: Material(
-                          shape: const CircleBorder(),
-                          elevation: 8,
-                          child: Container(
-                            padding: const EdgeInsets.all(40),
-                            height: 200,
-                            width: 200,
-                            decoration: const BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: Color.fromARGB(255, 140, 80, 182),
-                            ),
-                            child: const Icon(
-                              Icons.mic,
-                              color: Colors.white,
-                              size: 100,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  color: const Color.fromARGB(255, 89, 0, 89),
-                  height: MediaQuery.of(context).size.height,
-                  width: MediaQuery.of(context).size.width,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const Text(
-                        'Upload File',
-                        style: TextStyle(color: Colors.white, fontSize: 20, decoration: TextDecoration.none),
-                      ),
-                      const SizedBox(
-                        height: 40,
-                      ),
-                      GestureDetector(
-                        onTap: _openFileExplorer,
-                        child: Material(
-                          shape: const CircleBorder(),
-                          elevation: 8,
-                          child: Container(
-                            padding: const EdgeInsets.all(40),
-                            height: 200,
-                            width: 200,
-                            decoration: const BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: Color.fromARGB(255, 140, 80, 182),
-                            ),
-                            child: const Icon(
-                              Icons.add,
-                              color: Colors.white,
-                              size: 100,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          const Text('safasfasfasfasfsafs'),
-        ],
-      ),
-    );
-  }
-
-  // This method builds the dot for the 3 dots below the status bar
-  Container buildDot(int index, BuildContext context) {
-    return Container(
-      height: 10,
-      width: currentIndex == index ? 30 : 10,
-      margin: const EdgeInsets.only(right: 10),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: Colors.white,
-      ),
-    );
   }
 }
