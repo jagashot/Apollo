1bc2b447445d2a51426a3660fa3c717fcbdf5d52
YehudaDaniel <h10ytguf@gmail.com>
2024-08-21 22:50:39 +0300

    adding state to the record view page

--- lib/widgets/buildRecordView.widgets.dart
+++ lib/widgets/buildRecordView.widgets.dart
@@ -1,55 +1,65 @@
-
 import 'package:flutter/material.dart';
 import 'package:avatar_glow/avatar_glow.dart';
 
-Widget buildRecordView(BuildContext context) {
-  //Record View for the second page
-  return  Container(
-    height: MediaQuery.of(context).size.height,
-    width: MediaQuery.of(context).size.width,
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        const Text(
-          'Tap to Record',
-          style: TextStyle(color: Colors.white, fontSize: 20, decoration: TextDecoration.none),
-        ),
-        const SizedBox(
-          height: 40,
-        ),
-        AvatarGlow(
-          glowCount: 3,
-          glowRadiusFactor: 0.3,
-          //add state to the widget, turn off the animation when in another view
-          animate: true,
-          //----------------------------------------------------------------
-          child: GestureDetector(
-            onTap: () => print("Yea!"),
-            child: Material(
-              shape: const CircleBorder(),
-              elevation: 8,
-              child: Container(
-                padding: const EdgeInsets.all(40),
-                height: 200,
-                width: 200,
-                decoration: const BoxDecoration(
-                  image: DecorationImage(image:AssetImage('assets/photos/main_btn.png'), 
-                  fit: BoxFit.fill
+class BuildRecordView extends StatefulWidget {
+  const BuildRecordView({super.key});
+
+  @override
+  _BuildRecordViewState createState() => _BuildRecordViewState();
+}
+
+class _BuildRecordViewState extends State<BuildRecordView> {
+
+  @override
+  Widget build(BuildContext context) {
+    //Record View for the second page
+    return  Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Text(
+            'Tap to Record',
+            style: TextStyle(color: Colors.white, fontSize: 20, decoration: TextDecoration.none),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          AvatarGlow(
+            glowCount: 3,
+            glowRadiusFactor: 0.3,
+            //add state to the widget, turn off the animation when in another view
+            animate: true,
+            //----------------------------------------------------------------
+            child: GestureDetector(
+              onTap: () => print("Yea!"),
+              child: Material(
+                shape: const CircleBorder(),
+                elevation: 8,
+                child: Container(
+                  padding: const EdgeInsets.all(40),
+                  height: 200,
+                  width: 200,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(image:AssetImage('assets/photos/main_btn.png'), 
+                    fit: BoxFit.fill
+                    ),
+                    shape: BoxShape.circle,
+                    color: Color.fromARGB(255, 140, 80, 182),
                   ),
-                  shape: BoxShape.circle,
-                  color: Color.fromARGB(255, 140, 80, 182),
-                ),
-                child: const Icon(
-                  Icons.mic,
-                  color: Colors.white,
-                  size: 100,
+                  child: const Icon(
+                    Icons.mic,
+                    color: Colors.white,
+                    size: 100,
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-      ],
-    ),
-  );
+        ],
+      ),
+    );
+  }
 }
